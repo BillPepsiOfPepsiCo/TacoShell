0d805d5c1182878f47c3c44aa7c3423060547718
BillPepsi <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2019-02-10 14:35:13 -0600

    Added shell entry point logic.

--- shell.c
+++ shell.c
@@ -2,4 +2,19 @@
 // Created by bzane on 2/10/2019.
 //
 
+#include <stdio.h>
+#include <unistd.h>
 #include "shell.h"
+#include "command.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+void enter_shell() {
+    chdir(WORKING_DIR);
+
+    while (1) {
+        printf("%s@%s> ", WORKING_DIR, getlogin());
+        (exit_cmd->function)(NULL); //Test struct inits
+    }
+}
+#pragma clang diagnostic pop
--- shell.h
+++ shell.h
@@ -4,5 +4,8 @@
 
 #ifndef TECHSHELL_SHELL_H
 #define TECHSHELL_SHELL_H
+
 static char* WORKING_DIR = "~";
+void enter_shell(void);
+
 #endif //TECHSHELL_SHELL_H
