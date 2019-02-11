36dcc97b8cd40b48a4a8f33fc7aa27956626e98d
BillPepsi <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2019-02-11 00:56:34 -0600

    Added OS checking preprocessor macro

--- shell.h
+++ shell.h
@@ -5,7 +5,24 @@
 #ifndef TECHSHELL_SHELL_H
 #define TECHSHELL_SHELL_H
 
-static char* WORKING_DIR = "~";
+#define GET_WINDOWS_HOMEPATH 0
+#define GET_LINUX_HOMEPATH 0
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+#undef GET_WINDOWS_HOMEPATH
+#define GET_WINDOWS_HOMEPATH 1
+#define PATH_DELIM "\\"
+#endif //_WIN32
+
+#if defined(unix) || defined(__unix__) || defined (__unix)
+#undef GET_UNIX_HOMEPATH
+#define GET_UNIX_HOMEPATH 1
+#define PATH_DELIM "/"
+#endif //unix/__unix__/__unix
+
+static char* WORKING_DIR = "ERR";
+
 void enter_shell(void);
+int process_external_command(char*[]);
 
 #endif //TECHSHELL_SHELL_H
