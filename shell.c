3005636fc5611d16a500854f651be4bbba9c33bd
BillPepsi <5006946+BillPepsiOfPepsiCo@users.noreply.github.com>
2019-02-13 14:36:31 -0600

    * Remove WORKING_DIR global variable. The OS can keep track of this for me. * Shell now sets working directory correctly on startup. CD now works as well. * Added tokenizer code (simplified a little bit from the PR). * Added external command execution code, straight from the PR. (Thanks Devon!) * Added a million debug messages. They can be en/disabled at the bottom of shell.h.

--- shell.c
+++ shell.c
@@ -6,46 +6,104 @@
 #include <memory.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <wait.h>
 #include "shell.h"
 #include "command.h"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+int NUM_TOKENS = 0;
+
 void enter_shell() {
     //Update WORKING_DIR
 
     if (GET_WINDOWS_HOMEPATH) {
-        WORKING_DIR = getenv("HOMEPATH");
+        chdir(getenv("HOMEPATH"));
     } else if (GET_UNIX_HOMEPATH) {
-
+        chdir("~");
     }
-
-    chdir(WORKING_DIR);
 
     char buf[1024];
-    char* tokenized_arguments[16];
-    unsigned int token_index = 0;
+    char pathbuf[256];
+
+    char **tokenized_input = (char **) malloc(sizeof(char *) * 16);
+    for (int i = 0; i < 16; i++) {
+        tokenized_input[0] = (char *) malloc(sizeof(char) * 64);
+    }
 
     while (1) {
-        printf("%s@%s> ", WORKING_DIR, getlogin());
+        db_printf("enter_shell 1");
+        getcwd(pathbuf, sizeof(pathbuf));
+        printf("%s@%s> ", pathbuf, getlogin());
+        db_printf("enter_shell 2");
         gets(buf);
-
-        char* token;
-        char* remainder = buf;
-
-        while ((token = strtok_r(remainder, " ", &remainder))) {
-            tokenized_arguments[token_index] = token;
-            token_index++;
-        }
-
-        parse(tokenized_arguments);
-        token_index = 0;
-        memset(tokenized_arguments, 0, sizeof(tokenized_arguments));
+        db_printf("enter_shell 3");
+        parse_cl(buf, tokenized_input);
+        db_printf("enter_shell 4");
+        execcmd(tokenized_input);
     }
 }
+
+
 #pragma clang diagnostic pop
 
-int process_external_command(char* args[]) {
+//BEGIN DEVIN KNUD-SECTION
+void parse_cl(char input[], char **ret) {
+    unsigned int tok_index = 0;
+    char *token;
+    char *remainder = input;
 
+    while ((token = strtok_r(remainder, " ", &remainder))) {
+        ret[tok_index] = token;
+        tok_index++;
+    }
+
+    NUM_TOKENS = tok_index;
 }
 
+void execcmd(char **command) {
+    //Relatively complex macro.
+    //Very legal and very cool.
+    //All you need to know is that this expands to a macro
+    //that individually checks all 3 structs in command.h
+    //to see if command[0] matches any of their cmd_str's.
+    //If it does, it runs that command and then returns early.
+    chk_builtins();
+
+    db_printf("Execcmd 2");
+
+    if (fork() == 0) {
+        db_printf("Execcmd 2a");
+        char *cmd[NUM_TOKENS];
+        cmd[NUM_TOKENS] = NULL;
+        for (int i = 0; i < NUM_TOKENS; i++) {
+            cmd[i] = command[i];
+        }
+
+        db_printf("Execcmd 2b");
+
+        if (execvp(command[0], cmd) == -1) {
+            perror("Execution Error");
+            exit(EXIT_FAILURE);
+        }
+
+        db_printf("Execcmd 2c");
+    } else {
+        db_printf("Execcmd 3");
+        // wait for the child process to end
+        int status;
+        int exStat = 0;
+        if (waitpid(-1, &status, 0) == -1) {
+            // if the child process terminates incorrectly, exit status: 1
+            exStat = WEXITSTATUS(status);
+        } else {
+            // if the child process terminates correctly, exit status: 0
+            exStat = WEXITSTATUS(status);
+        }
+
+        if (DEBUG)
+            printf("Exit Status: %d\n", exStat);
+        db_printf("Execcmd 3a");
+    }
+}
--- shell.h
+++ shell.h
@@ -5,24 +5,35 @@
 #ifndef TECHSHELL_SHELL_H
 #define TECHSHELL_SHELL_H
 
+//Initial DEFINE values for OS detection
 #define GET_WINDOWS_HOMEPATH 0
 #define GET_LINUX_HOMEPATH 0
 
+//For any version of windows, including Cygwin dev environments
 #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
 #undef GET_WINDOWS_HOMEPATH
 #define GET_WINDOWS_HOMEPATH 1
-#define PATH_DELIM "\\"
 #endif //_WIN32
 
+//For any version of *nix
 #if defined(unix) || defined(__unix__) || defined (__unix)
 #undef GET_UNIX_HOMEPATH
 #define GET_UNIX_HOMEPATH 1
-#define PATH_DELIM "/"
 #endif //unix/__unix__/__unix
 
-static char* WORKING_DIR = "ERR";
-
 void enter_shell(void);
-int process_external_command(char*[]);
+void parse_cl(char[], char*[]);
+void execcmd(char**);
+
+//Useful macros
+#define command_compare(s) strcmp(command[0], s->cmd_str) == 0
+#define call(cmd) (cmd->function)((const char**) command);
+#define chk_builtin(command) if (command_compare(command)) { call(command); return; }
+#define chk_builtins() chk_builtin(p_wd); chk_builtin(cd); chk_builtin(exit_cmd);
+
+#define db_printf(s) if (DEBUG) { printf("%s\n", s); }
+
+//DEBUG macro for this file
+#define DEBUG 0
 
 #endif //TECHSHELL_SHELL_H
